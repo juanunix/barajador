bf8a27780553f0a33bdabc137c47339cd59c03f2
eypacha <pachaguionbajo@gmail.com>
2015-10-10 13:49:43 -0300

    agregado de botonera

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ var imgBack = "bicycle-red";
 var vrChck0 = "glyphicon glyphicon-unchecked";
 var vrChck1 = "glyphicon glyphicon-check"
 
-
 var JsonApi = new RandomJs();
 
 // Inicia el menu de herramientas
@@ -29,29 +28,30 @@ $( "#colorConsolaTexto" ).on( "change", cambiarColorConsolaTexto );
 $( "#fuenteConsola" ).on( "change", cambiarFuenteConsola );
 
 // Menu: Mezclar
-$( "#sfInvertir" ).on( "click", sfInvertir );
-$( "#sfFisherYates" ).on( "click", sfFisherYates );
-$( "#sfSattolo" ).on( "click", sfSattolo );
-$( "#sfRandomOrg" ).on( "click", sfRandomOrg );
-$( "#sfCortar" ).on( "click", sfCortar );
-$( "#sfOverhand" ).on( "click", sfOverhand );
-$( "#sfFaroExt" ).on( "click", sfFaroExt );
-$( "#sfFaroIn" ).on( "click", sfFaroInt );
-$( "#sfAntiFaroExt" ).on( "click", sfAntiFaroExt );
-$( "#sfAntiFaroIn" ).on( "click", sfAntiFaroInt );
+$( ".sfInvertir" ).on( "click", sfInvertir );
+$( ".sfFisherYates" ).on( "click", sfFisherYates );
+$( ".sfSattolo" ).on( "click", sfSattolo );
+$( ".sfRandomOrg" ).on( "click", sfRandomOrg );
+$( ".sfFaroExt" ).on( "click", sfFaroExt );
+$( ".sfFaroIn" ).on( "click", sfFaroInt );
+$( ".sfAntiFaroExt" ).on( "click", sfAntiFaroExt );
+$( ".sfAntiFaroIn" ).on( "click", sfAntiFaroInt );
+// $( "#sfCortar" ).on( "click", sfCortar );
+// $( "#sfOverhand" ).on( "click", sfOverhand );
 
 // Menu: Ver Check / UnCheck
 $( "#vrMatriz" ).on( "click", {name: "Matriz"}, verModulos );
 $( "#vrTapete" ).on( "click", {name: "Tapete"}, verModulos );
+$( "#vrBotonera" ).on( "click", {name: "Botonera"}, verModulos );
 $( "#vrConsola" ).on( "click", {name: "Consola"}, verModulos );
 
 // Ordenaciones
-var baraja = "AS,2S,3S,4S,5S,6S,7S,8S,9S,10S,JS,QS,KS,AD,2D,3D,4D,5D,6D,7D,8D,9D,10D,JD,QD,KD,AC,2C,3C,4C,5C,6C,7C,8C,9C,10C,JC,QC,KC,AH,2H,3H,4H,5H,6H,7H,8H,9H,10H,JH,QH,KH";
+var baraja = "KH,QH,JH,10H,9H,8H,7H,6H,5H,4H,3H,2H,AH,KC,QC,JC,10C,9C,8C,7C,6C,5C,4C,3C,2C,AC,KD,QD,JD,10D,9D,8D,7D,6D,5D,4D,3D,2D,AD,KS,QS,JS,10S,9S,8S,7S,6S,5S,4S,3S,2S,AS";
 baraja = baraja.split(",");
 
 // Ordenar Bicycle
 function ordenarBicycle(){
-    baraja = "AS,2S,3S,4S,5S,6S,7S,8S,9S,10S,JS,QS,KS,AD,2D,3D,4D,5D,6D,7D,8D,9D,10D,JD,QD,KD,KC,QC,JC,10C,9C,8C,7C,6C,5C,4C,3C,2C,AC,KH,QH,JH,10H,9H,8H,7H,6H,5H,4H,3H,2H,AH";
+    baraja = "AH,2H,3H,4H,5H,6H,7H,8H,9H,10H,JH,QH,KH,AC,2C,3C,4C,5C,6C,7C,8C,9C,10C,JC,QC,KC,KD,QD,JD,10D,9D,8D,7D,6D,5D,4D,3D,2D,AD,KS,QS,JS,10S,9S,8S,7S,6S,5S,4S,3S,2S,AS";
     baraja = baraja.split(",");
     abreBaraja();
 }
@@ -332,12 +332,12 @@ function generarQr(){
 }
 
 function cambiarColorTapete(){
-    $('.contenedor').css('background-color',$('#colorTapete').val())
+    $('#tapeteFondo').css('background-color',$('#colorTapete').val())
 }
 
 function cambiarTexturaTapete(){
     var urlTextura = "url('img/table/"+ $('#texturaTapete').val() + ".png')"
-    $('.contenedor').css('background-image',urlTextura)
+    $('#tapeteFondo').css('background-image',urlTextura)
 }
 
 function cambiarColorConsola(){
