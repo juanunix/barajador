187ff39c00870c1fa04ac158330682811142b8fb
eypacha <pachaguionbajo@gmail.com>
2015-10-16 22:07:30 -0300

    Implementación del algoritmo Fisher-Yates original

--- main.js
+++ main.js
@@ -31,6 +31,7 @@ $( "#fuenteConsola" ).on( "change", cambiarFuenteConsola );
 // Menu: Mezclar
 $( ".sfInvertir" ).on( "click", sfInvertir );
 $( ".sfFisherYates" ).on( "click", sfFisherYates );
+$( ".sfDurstenfeld" ).on( "click", sfDurstenfeld );
 $( ".sfSattolo" ).on( "click", sfSattolo );
 $( ".sfRandomOrg" ).on( "click", sfRandomOrg );
 $( ".sfFaroExt" ).on( "click", sfFaroExt );
@@ -56,6 +57,7 @@ $( "#vrConsola" ).on( "click", {name: "Consola"}, verModulos );
 
 
 document.oncontextmenu = function(){return false;}
+
 
 // Ordenaciones
 var baraja = "AC,2C,3C,4C,5C,6C,7C,8C,9C,10C,JC,QC,KC,AH,2H,3H,4H,5H,6H,7H,8H,9H,10H,JH,QH,KH,AS,2S,3S,4S,5S,6S,7S,8S,9S,10S,JS,QS,KS,AD,2D,3D,4D,5D,6D,7D,8D,9D,10D,JD,QD,KD";
@@ -128,8 +130,27 @@ function abreBaraja(){
     renderizar();
 }
 
-// Algoritmo de FisherShuttle 
 function sfFisherYates() {
+    var barajaTemp = baraja.slice();
+    var n = baraja.length;
+    var i;
+    baraja = [];
+  // While there remain elements to shuffle…
+  while (n) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * n--);
+
+    // And move it to the new array.
+    baraja.push(barajaTemp.splice(i, 1)[0]);
+  }
+    
+  renderizar();
+  consola("fisherYates");
+}
+
+// Algoritmo de Durstenfeld
+function sfDurstenfeld() {
     var counter = baraja.length, temp, index;
 
     // While there are elements in the array
@@ -428,14 +449,19 @@ function ejecutarComando(texto){
                 sfInvertir();
                 return;
             }
-            case "sattolo":
+            case "fisheryates":
             {
-                sfSattolo();
+                sfFisherYates();
                 return;
             }
             case "durstenfeld":
             {
-                sfFisherYates();
+                sfDurstenfeld();
+                return;
+            }
+            case "sattolo":
+            {
+                sfSattolo();
                 return;
             }
             case "randomorg":
